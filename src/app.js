4f1c1399f86386efc840998c92588a9fd3d852c7
Marcio Rodrigues <marcio.silva@CLUBRNB1098>
2019-09-18 13:08:05 -0300

    bug(actions): actions must be plain object?
    Co-authored-by: Marcio Mafideju <mafideju@outlook.com>

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 'use strict'
 
 import React from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from './redux-flow/actions'
 
-const App = () => (
+const App = ({ todos, handleSubmit }) => (
   <div>
-    <input type='text' />
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='todo' />
+      <button type='submit'>Enviar</button>
+    </form>
+    {console.log(todos)}
     <ul>
-      <li style={{ textDecoration: 'line-through' }}>Item 1</li>
+      <li>Item 1</li>
       <li>Item 2</li>
-      <li>Item 3</li>
+      <li style={{ textDecoration: 'line-through' }}>Item 3</li>
       <li>Item 4</li>
       <li>Item 5</li>
     </ul>
@@ -18,4 +24,12 @@ const App = () => (
   </div>
 )
 
-export default App
+const mapStateToProps = (state) => ({ todos: state })
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmit: (e) => {
+    e.preventDefault()
+    dispatch(addTodo('Hey'))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
